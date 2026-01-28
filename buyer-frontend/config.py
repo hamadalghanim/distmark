bd2a7cc5ae07e5fdd0151143b1ea38ee602521ae
PKTidus <pedrotk@fastmail.com>
2026-01-28 15:04:36 -0700

    added native postgres driver

--- buyer-frontend/config.py
+++ buyer-frontend/config.py
@@ -2,10 +2,10 @@ from sqlalchemy import create_engine
 from db import BaseCustomers
 
 products_engine = create_engine(
-    "postgresql://user:pass@products-database:5432/products", echo=True
+    "postgresql+psycopg2://user:pass@products-database:5432/products", echo=True
 )
 
 customers_engine = create_engine(
-    "postgresql://user:pass@customers-database:5432/customers", echo=True
+    "postgresql+psycopg2://user:pass@customers-database:5432/customers", echo=True
 )
 BaseCustomers.metadata.create_all(customers_engine)
--- seller-frontend/config.py
+++ seller-frontend/config.py
@@ -2,16 +2,8 @@ from sqlalchemy import create_engine
 from db import BaseProducts
 from seed import seed
 
-# customer_db = DatabaseConnection(
-#     db_name="customers",
-#     user="user",
-#     password="pass",
-#     host="customers-database",  # Updated to connect to the correct database service
-#     port=5432,
-# )
-# customer_db.connect()
 products_engine = create_engine(
-    "postgresql://user:pass@products-database:5432/products", echo=True
+    "postgresql+psycopg2://user:pass@products-database:5432/products", echo=True
 )
 
 seed(products_engine)
